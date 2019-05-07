399a477ac9326c353f3a2672d3c6de8ea7aa9087
John Cooper <johnxcooper87@gmail.com>
2019-05-07 13:59:50 +0700

    update

--- src/classes/Mutation.js
+++ src/classes/Mutation.js
@@ -24,7 +24,10 @@ export class Mutation {
         }
       });
       if (this.blockUI) UIBlocker.enable();
-      this.onComplete?.(data, variables);
+      this.onComplete?.(data, {
+        ...this.static,
+        ...variables
+      });
 
     } catch (error) {
       if (this.blockUI) UIBlocker.enable();
