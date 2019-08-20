22c1188faeaec7dcd42c9f8f28d7e4123e4e3da2
John Cooper <johnxcooper87@gmail.com>
2019-08-20 18:14:05 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export { GraphqlClient } from './src/classes/GraphqlClient';
-export { Mutation } from './src/classes/Mutation';
+export { GraphqlClient } from './src/lib/GraphqlClient';
+export { Mutation } from './src/lib/Mutation';
 export { useQuery } from './src/hooks/useQuery';
 export { useMutation } from './src/hooks/useMutation';
-export { refetch } from './src/classes/refetch';
+export { refetch } from './src/lib/refetch';
